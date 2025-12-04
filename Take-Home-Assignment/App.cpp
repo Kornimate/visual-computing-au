d2bc5496d7a0e712db1bdfacb74a5eb3ebc84deb
Kornimate <kornimate@gmail.com>
2025-12-04 19:19:52 +0100

    report proceeded, FPS counter added

--- Take-Home-Assignment/App.cpp
+++ Take-Home-Assignment/App.cpp
@@ -23,34 +23,46 @@ App::~App() {
 }
 
 void App::initialize() {
-    if (!glfwInit()) {
-        throw "Failed to init GLFW!";
-    }
+	if (!glfwInit()) {
+		throw "Failed to init GLFW!";
+	}
 
 	App::gDynamicMeshes.reserve(1000); // initialize capaciyt to avoid reallocation data loss
 	App::gObjects.reserve(1000);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // 1. Create the FIRST window (2D)
-    this->_win2d->initialize();
+	// 1. Create the FIRST window (2D)
+	this->_win2d->initialize();
 
-    // 3. Create the 3D window and share context with 2D
-    this->_win3d->initialize(this->_win2d->getWindowInstance());
+	// 3. Create the 3D window and share context with 2D
+	this->_win3d->initialize(this->_win2d->getWindowInstance());
 
 	glfwMakeContextCurrent(this->_win2d->getWindowInstance());
 }
 
 
 void App::run() {
+	auto t0 = std::chrono::steady_clock::now();
+	int frames = 0;
 
 	while (!glfwWindowShouldClose(this->_win2d->getWindowInstance()) &&
 		!glfwWindowShouldClose(this->_win3d->getWindowInstance())) {
-		
+
 		this->_win2d->run();
 		this->_win3d->run();
+
+		frames++;
+
+		auto t1 = std::chrono::steady_clock::now();
+		if (std::chrono::duration_cast<std::chrono::seconds>(t1 - t0).count() >= 1) {
+
+			std::cout << "FPS: " << frames << ", # objects: " << App::gObjects.size() << std::endl;
+
+			frames = 0; t0 = t1;
+		}
 
 		glfwPollEvents();
 	}
